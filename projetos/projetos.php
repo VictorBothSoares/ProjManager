80fcdebfecc62729cda1ba48c69b54bf82e1a2e1
VictorBothSoares <victorsoares@ienh.com.br>
2023-05-28 20:05:56 -0300

    Meio cadastro Projeto realizado

--- projetos/projetos.php
+++ projetos/projetos.php
@@ -8,48 +8,100 @@
     <link rel="stylesheet" href="projetosstyle.css">
 </head>
 <body>
-    <nav>
-        <div class="navegacao"></div>
-        <img class="Menu" src="../Fotos/Menu.png" alt="Menu"></button>
-        <a href="../index.html"><title class="capa">ProjManager</title></a>
-    </nav>
-    <section>
-        <div>
-        <p><strong>Cod. Projeto:
-            <input type="number" name="" min="0">
-        </strong>
-    </p>
-        <p class="botao">
-            <input type="button" value="Buscar" onclick="">
-        </p>
-        </div>
-    </section>
-    <h1>Cadastro Projeto</h1>
-    <p class="Nome">Nome do Projeto:
-        <input type="" name="" min="0">
-    </p>
-    <p class="verba">Verba:
-        <input type="" name="" min="0">
-    </p>
-    <p class="valorg">Valor gasto:
-        <input type="" name="" min="0">
-    </p>
-    <p class="desc">Descrição do Projeto:</p>
-        <textarea name="" id="descprojeto" cols="60" rows="15"></textarea>
+    <header>
+        <nav class="navegacao">
+        </nav>
+        <a href="../login.html">
+            <p class="sair">Sair</p>
+        </a>
+        <a href="../index.html">
+            <h1 class="capa">ProjManager</h1>
+        </a>
+    </header>
 
-    <p class="nomec">Nome Cliente:
-        <input type="" name="" min="0">
-    </p>
-    <p class="resp">Nome do Responsável:
-        <input type="" name="" min="0">
-    </p>
-    <p class="datecriacao">Data Criação:
-        <input type="date" name="" min="0">
-    </p>
-    <p class="dateconclusao">Data Conclusão:
-        <input type="date" name="" min="0">
-    </p>
-    <p class="Salvar">
-        <input type="button" value="Salvar" onclick="">
-    </p>
+    <section class="projetos">
+        <form method="get" action="projetos.php">
+            <div>
+                <label for="busca"><strong>Código do Projeto:</strong></label>
+                <input type="text" id="busca" name="busca" required>
+            </div>
+            <div class="botao">
+                <input type="submit" value="Buscar">
+            </div>
+        </form>
+    </section>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        // Configurações do banco de dados
+        $host = 'localhost';
+        $dbname = 'mydb';
+        $username = 'root';
+        $password = '';
+
+        try {
+            // Conexão com o banco de dados
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Obter o valor do campo de busca
+            $busca = $_GET['busca'];
+
+            // Verificar se o valor é um número (código do projeto)
+            if (preg_match('/^\d+$/', $busca)) {
+                $idProjeto = $busca;
+                $sql = "SELECT * FROM projetos WHERE idProjetos = :idProjeto";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':idProjeto', $idProjeto, PDO::PARAM_INT);
+                $stmt->execute();
+
+                if ($stmt->rowCount() > 0) {
+                    // Obter os dados do projeto
+                    $projeto = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                    // Preencher os campos do formulário com os dados do projeto
+                    $nomeProjeto = $projeto['NomeProjeto'];
+                    $verba = $projeto['verba'];
+                    $valorGasto = $projeto['valorGasto'];
+                    $descricao = $projeto['Descricao'];
+                    $cliente_id = $projeto['Cliente_idCliente'];
+                    $dataInicio = $projeto['dataInicio'];
+                    $dataConclusao = $projeto['DataConclusao'];
+                } else {
+                    echo "Projeto não encontrado.";
+                }
+            }
+        } catch (PDOException $e) {
+            echo "Erro: " . $e->getMessage();
+        }
+    }
+    ?>
+
+    <h1>Cadastro Projeto</h1>
+    <form action="salvarProjetos.php" method="post">
+        <p class="Nome">Nome do Projeto:
+            <input type="text" name="nome_projeto" required value="<?php echo isset($nomeProjeto) ? $nomeProjeto : ''; ?>">
+        </p>
+        <p class="verba">Verba:
+            <input type="number" name="verba" min="0" required value="<?php echo isset($verba) ? $verba : ''; ?>">
+        </p>
+        <p class="valorg">Valor gasto:
+            <input type="number" name="valor_gasto" min="0" required value="<?php echo isset($valorGasto) ? $valorGasto : ''; ?>">
+        </p>
+        <p class="desc">Descrição do Projeto:</p>
+        <textarea name="descricao" id="descprojeto" cols="60" rows="15" required><?php echo isset($descricao) ? $descricao : ''; ?></textarea>
+        <p class="nomec">Código Cliente:
+            <input type="number" name="nome_cliente" required value="<?php echo isset($cliente_id) ? $cliente_id : ''; ?>">
+        </p>
+        <p class="datecriacao">Data Criação:
+            <input type="date" name="data_criacao" required value="<?php echo isset($dataInicio) ? $dataInicio : ''; ?>">
+        </p>
+        <p class="dateconclusao">Data Conclusão:
+            <input type="date" name="data_conclusao" required value="<?php echo isset($dataConclusao) ? $dataConclusao : ''; ?>">
+        </p>
+        <p class="Salvar">
+            <input type="submit" value="Salvar">
+        </p>
+    </form>
 </body>
+</html>
---
+++ projetos/salvarProjetos.php
@@ -0,0 +1,52 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Configurações do banco de dados
+    $host = 'localhost';
+    $dbname = 'mydb';
+    $username = 'root';
+    $password = '';
+
+    try {
+        // Conexão com o banco de dados
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Verifica se o NomeProjeto já existe no banco de dados
+        $nomeProjeto = $_POST['nome_projeto'];
+        $sqlSelect = "SELECT COUNT(*) as count FROM projetos WHERE NomeProjeto = :nome_projeto";
+        $stmtSelect = $conn->prepare($sqlSelect);
+        $stmtSelect->bindParam(':nome_projeto', $nomeProjeto);
+        $stmtSelect->execute();
+
+        $result = $stmtSelect->fetch(PDO::FETCH_ASSOC);
+        $count = $result['count'];
+
+        if ($count > 0) {
+            $sql = "UPDATE projetos SET verba = :verba, valorGasto = :valor_gasto, Descricao = :descricao, dataInicio = :data_inicio, DataConclusao = :data_conclusao WHERE NomeProjeto = :nome_projeto";
+        } else {
+            $sql = "INSERT INTO projetos (NomeProjeto, verba, valorGasto, Descricao, Cliente_idCliente, dataInicio, DataConclusao)
+            VALUES (:nome_projeto, :verba, :valor_gasto, :descricao, :cliente_id, :data_inicio, :data_conclusao)";
+        }
+
+        $stmt = $conn->prepare($sql);
+
+        // Parâmetros da inserção ou atualização
+        $params = array(
+            ':nome_projeto' => $_POST['nome_projeto'],
+            ':verba' => $_POST['verba'],
+            ':valor_gasto' => $_POST['valor_gasto'],
+            ':descricao' => $_POST['descricao'],
+            ':cliente_id' => $_POST['nome_cliente'],
+            ':data_inicio' => $_POST['data_criacao'],
+            ':data_conclusao' => $_POST['data_conclusao']
+        );
+
+        // Executa a inserção ou atualização
+        $stmt->execute($params);
+
+        echo 'Dados inseridos/atualizados com sucesso!';
+    } catch (PDOException $e) {
+        echo 'Erro ao inserir/atualizar os dados: ' . $e->getMessage();
+    }
+}
+?>
